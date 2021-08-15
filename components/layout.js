0e64bb0b47f1a8dceba4e48a4225bfc20417c224
likawoopitiki <blackalsatian@gmail.com>
2021-08-15 13:47:02 +0200

    Header changes.

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,8 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Developerating'
-export const siteTitle = 'Next.js Sample Website'
+const hostname = window.location.hostname
+export const siteTitle = 'Next.js Experiment'
 
 export default function Layout({ children, home }) {
   return (
@@ -14,13 +15,11 @@ export default function Layout({ children, home }) {
         <link rel="icon" href="/icon-48x48.png" />
         <meta
           name="description"
-          content="Learn how to build a personal website using Next.js"
+          content="A personal website using Next.js"
         />
         <meta
           property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+          content={`https://${hostname}/images/head-pic.jpg`}
         />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
