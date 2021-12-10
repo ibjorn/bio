61e1dba724fcaa849ee382f476766f5461ffa751
likawoopitiki <blackalsatian@gmail.com>
2021-12-10 22:38:54 +0200

    Added GTM tags.

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,48 @@
 import { AnimatePresence, motion } from 'framer-motion'
+import Script from 'next/script'
 import { useEffect } from 'react'
 import MainLayout from '../components/layouts/mainLayout'
+import { GTM_ID, pageview } from '../lib/gtm'
 import OffCanvasProvider from '../store/offCanvasProvider'
 import '../styles/global.scss'
 
 export default function App({ Component, pageProps, router }) {
   useEffect(() => {
-    const handleRouteChange = url => {
-      window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
-        page_path: url
-      })
-    }
-    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on('routeChangeComplete', pageview)
     return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
+      router.events.off('routeChangeComplete', pageview)
     }
   }, [router.events])
+  // old code for UA
+  // useEffect(() => {
+  //   const handleRouteChange = url => {
+  //     window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+  //       page_path: url
+  //     })
+  //   }
+  //   router.events.on('routeChangeComplete', handleRouteChange)
+  //   return () => {
+  //     router.events.off('routeChangeComplete', handleRouteChange)
+  //   }
+  // }, [router.events])
 
   const getLayout = Component.getLayout || (page => page)
   const useLayout = getLayout(<Component {...pageProps} />)
 
   return (
     <OffCanvasProvider>
+      <Script
+        strategy='afterInteractive'
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${GTM_ID}');
+          `
+        }}
+      />
       <AnimatePresence>
         <motion.div
           key={router.route}
--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,14 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document'
+import { GTM_ID } from '../lib/gtm'
 
 class AppDocument extends Document {
   render() {
     return (
       <Html lang='en'>
         <Head>
+          {/* 
+
+            // new GTM tag. trying differnt appraoch in _app.js
           <link rel='dns-prefetch' href='https://www.google-analytics.com' />
           <link rel='dns-prefetch' href='https://www.googletagmanager.com' />
           <link
@@ -17,12 +21,24 @@ class AppDocument extends Document {
             href='https://www.google-analytics.com'
             crossOrigin='true'
           />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${process.env.NEXT_PUBLIC_GTM}');`
+            }}
+          /> */}
           <link rel='preconnect' href='https://fonts.googleapis.com' />
           <link
             rel='preconnect'
             href='https://fonts.gstatic.com'
             crossOrigin='true'
           />
+          {/* 
+          // old UA tag
+          
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
@@ -36,13 +52,21 @@ class AppDocument extends Document {
               page_path: window.location.pathname,
             });`
             }}
-          />
+          /> */}
           <link
             href='https://fonts.googleapis.com/css2?family=Oswald:wght@200;400&family=Raleway:wght@400;800&display=swap'
             rel='stylesheet'
           />
         </Head>
         <body>
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+              height='0'
+              width='0'
+              style={{ display: 'none', visibility: 'hidden' }}
+            ></iframe>
+          </noscript>
           <Main />
           <NextScript />
         </body>
