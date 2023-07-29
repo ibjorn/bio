209b3a7060b407a61590d0e7d7ff3f6277e83f1a
ibjorn <bjorn@blackalsatian.co.za>
2023-07-29 10:44:23 +0000

    Fixed title fonts on Blog pages.

--- components/blog/blogIndex.js
+++ components/blog/blogIndex.js
@@ -1,18 +1,17 @@
 import Link from 'next/link'
+import { oswald } from '../../app/fonts'
 import Date from '../date'
 import { blogIndexWrapper, blogIntro } from '../layouts/layout.module.scss'
 
 export default function BlogIndex({ posts }) {
   return (
     <div className={`${blogIndexWrapper}`}>
-      <h1>Blog</h1>
+      <h1 className={oswald.className}>Blog</h1>
 
       {posts.map(({ id, date, title, description }) => (
         <article key={id} className={blogIntro}>
-          <h2>
-            <Link href={`/blog/${id}/`}>
-              {title}
-            </Link>
+          <h2 className={oswald.className}>
+            <Link href={`/blog/${id}/`}>{title}</Link>
           </h2>
           <small>
             <Date dateString={date} />
@@ -21,5 +20,5 @@ export default function BlogIndex({ posts }) {
         </article>
       ))}
     </div>
-  );
+  )
 }
--- components/blog/blogPost.js
+++ components/blog/blogPost.js
@@ -1,10 +1,11 @@
+import { oswald } from '../../app/fonts'
 import Date from '../date'
 import { blogPost } from '../layouts/layout.module.scss'
 
 const BlogPost = ({ title, date, content }) => {
   return (
     <article className={blogPost}>
-      <h1>{title}</h1>
+      <h1 className={oswald.className}>{title}</h1>
       <div>
         <small>
           <Date dateString={date} />
